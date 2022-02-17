e5abcb63f6ce08c93ad9fd1913a11ccbdd6a3d9f
Oleksii <shyliaiev@gmail.com>
2022-02-17 08:54:32 +0200

    Initial commit

---
+++ src/ua/edu/odaba/lecture1/Main.java
@@ -0,0 +1,13 @@
+package ua.edu.odaba.lecture1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        println("Hello world!");
+    }
+
+    public static void println(String string) {
+        System.out.println(string);
+    }
+}
+
