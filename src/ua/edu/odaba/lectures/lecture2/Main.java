12a648557694b056138df6089694e9b24bb8729b
Oleksii <shyliaiev@gmail.com>
2022-02-23 09:55:49 +0200

    Merged mx file

---
+++ src/ua/edu/odaba/lectures/lecture2/Main.java
@@ -0,0 +1,22 @@
+package ua.edu.odaba.lectures.lecture2;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 12;
+
+        int c = a + b;
+
+        System.out.println(a);
+        System.out.println(a + b);
+        System.out.println(c);
+
+        c++;
+        System.out.println(c);
+        c--;
+        System.out.println(c);
+
+        c *= 12;
+        System.out.println(c);
+    }
+}
