532e4c891176389c41607fab7bde366f00dd5c98
BuildTools <unconfigured@null.spigotmc.org>
2022-02-23 09:40:04 +0200

    1st practice

--- src/ua/edu/odaba/practices/_1_helloworld/HelloWorld.java
+++ src/ua/edu/odaba/practices/_1_helloworld/HelloWorld.java
@@ -3,7 +3,6 @@ package ua.edu.odaba.practices._1_helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        //put your code here
-
+        System.out.println("HelloWorld");
     }
 }
