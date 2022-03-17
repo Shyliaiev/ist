4f0598458531de8c362cac910ab16708ab65551e
oleksii.shyliaiev <shyliaiev@gmail.com>
2022-03-17 09:18:47 +0200

    Second task done

--- src/ua/edu/odaba/practices/_2_meet_a_stranger/MeetAStranger.java
+++ src/ua/edu/odaba/practices/_2_meet_a_stranger/MeetAStranger.java
@@ -1,9 +1,13 @@
 package ua.edu.odaba.practices._2_meet_a_stranger;
 
+import java.util.Scanner;
+
 public class MeetAStranger {
     public static void main(String[] args) {
+//        System.out.print("Please, enter some text: \n");
+        Scanner scanner = new Scanner(System.in);
 
-        //put your code here
-
+        String readInput = scanner.nextLine();
+        System.out.println("Hello, " + readInput);
     }
 }
