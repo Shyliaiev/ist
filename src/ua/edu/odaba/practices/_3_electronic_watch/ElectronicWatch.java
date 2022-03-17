8efca909b5bd37e069a7c79186f58f3cdae169ff
oleksii.shyliaiev <shyliaiev@gmail.com>
2022-03-17 09:47:53 +0200

    Third task started

--- src/ua/edu/odaba/practices/_3_electronic_watch/ElectronicWatch.java
+++ src/ua/edu/odaba/practices/_3_electronic_watch/ElectronicWatch.java
@@ -1,9 +1,17 @@
 package ua.edu.odaba.practices._3_electronic_watch;
 
+import java.util.Scanner;
+
 public class ElectronicWatch {
     public static void main(String[] args) {
 
-        //put your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter integer");
 
+        int lon = scanner.nextInt();
     }
 }
+
+
+//1. Отримати на вхід ціле число
+//2. Надрукувати вивід екрану електронного годинника
