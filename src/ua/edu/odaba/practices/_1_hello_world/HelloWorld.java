1815f8675e8667b096f6a100bd8014e664a21100
oleksii.shyliaiev <shyliaiev@gmail.com>
2022-03-17 08:55:04 +0200

    First task done

--- src/ua/edu/odaba/practices/_1_hello_world/HelloWorld.java
+++ src/ua/edu/odaba/practices/_1_hello_world/HelloWorld.java
@@ -3,7 +3,7 @@ package ua.edu.odaba.practices._1_hello_world;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        //put your code here
+        System.out.println("Hello, World!");
 
     }
 }
