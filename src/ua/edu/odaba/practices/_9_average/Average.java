527b844631c686d7aeb0c4e175360dcbe8f2fde6
oleksii.shyliaiev <shyliaiev@gmail.com>
2022-04-14 09:44:52 +0300

    Average ready

--- src/ua/edu/odaba/practices/_9_average/Average.java
+++ src/ua/edu/odaba/practices/_9_average/Average.java
@@ -1,9 +1,26 @@
 package ua.edu.odaba.practices._9_average;
 
+import java.util.Scanner;
+
 public class Average {
     public static void main(String[] args) {
 
-        //put your code here
+        Scanner scanner = new Scanner(System.in);
+        int item;
+        int sum = 0;
+        int count = 0;
+
+        while (true) {
+            item = scanner.nextInt();
+            if (item == 0) {
+                break;
+            }
+            sum += item;
+
+            count++;
+        }
+
+        System.out.println(sum / count);
 
     }
 }
