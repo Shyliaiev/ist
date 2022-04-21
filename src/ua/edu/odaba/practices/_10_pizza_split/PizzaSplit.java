b5ad3593f54630327e06adf56f00e61cfceeef04
oleksii.shyliaiev <shyliaiev@gmail.com>
2022-04-21 09:47:02 +0300

    Practice with pizza solved

--- src/ua/edu/odaba/practices/_10_pizza_split/PizzaSplit.java
+++ src/ua/edu/odaba/practices/_10_pizza_split/PizzaSplit.java
@@ -1,9 +1,27 @@
 package ua.edu.odaba.practices._10_pizza_split;
 
+import java.util.Scanner;
+
 public class PizzaSplit {
     public static void main(String[] args) {
 
-        //put your code here
+        int people;
+        int slicesInOnePizza;
+        int numberOfPizzas = 1;
 
+        Scanner scanner = new Scanner(System.in);
+
+        people = scanner.nextInt();
+        slicesInOnePizza = scanner.nextInt();
+
+        while (numberOfPizzas * slicesInOnePizza % people != 0) {
+            numberOfPizzas++;
+        }
+
+        System.out.println(numberOfPizzas);
+
+//хватает ли всех кусочков всех заказанныъ пицц чтобы
+//                поделить между всеми людьми поровну и
+//                без остатка
     }
 }
