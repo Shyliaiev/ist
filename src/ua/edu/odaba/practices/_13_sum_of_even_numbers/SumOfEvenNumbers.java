14c35baa48cea214d4a61d93f22a5564e94d5d14
oleksii.shyliaiev <shyliaiev@gmail.com>
2022-05-05 09:25:02 +0300

    Sum of even numbers done

--- src/ua/edu/odaba/practices/_13_sum_of_even_numbers/SumOfEvenNumbers.java
+++ src/ua/edu/odaba/practices/_13_sum_of_even_numbers/SumOfEvenNumbers.java
@@ -1,25 +1,25 @@
 package ua.edu.odaba.practices._13_sum_of_even_numbers;
 
-
 public class SumOfEvenNumbers {
 
     public static void main(String[] args) {
-        int[] array = new int[]{1, 3, 2, 8, 15, 199};
-
+        int[] array = new int[]{1, 3, -6, -4, 15, 199};
+//        System.out.println(Arrays.toString(array));
         System.out.println(sum(array));
+//        System.out.println(Arrays.toString(array));
     }
 
     public static int sum(int[] array){
         int sum = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+        if (array == null) {
+            return 0;
         }
 
-        if (sum % 2 == 0) {
-            System.out.println("number " + n + " even");
-        } else {
-            System.out.println("number " + n + " odd");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                sum += array[i];
+            }
         }
 
         return sum;
