abbfea0b356e8ec931f90cebd708318881c28832
oleksii.shyliaiev <shyliaiev@gmail.com>
2022-05-05 09:42:50 +0300

    Sum of previous done

--- src/ua/edu/odaba/practices/_14_sum_of_previous/SumOfPrevious.java
+++ src/ua/edu/odaba/practices/_14_sum_of_previous/SumOfPrevious.java
@@ -5,16 +5,26 @@ import java.util.Arrays;
 public class SumOfPrevious {
 
     public static void main(String[] args) {
-        int[] array = new int[]{1, -1, 0, 4, 6, 10, 15, 25};
+        int[] array = new int[]{-1, -1, 0, 4, 6, 10, 15, 25};
 
         System.out.println(Arrays.toString(getSumCheckArray(array)));
     }
 
     public static boolean[] getSumCheckArray(int[] array){
 
-        //put your code here
+        boolean[] array2 = new boolean[array.length];
 
-        throw new UnsupportedOperationException();
+        array2[0] = false;
+        array2[1] = false;
 
+        for (int i = 2; i < array.length; i++) {
+            if (array[i - 1] + array[i - 2]  == array[i]) {
+                array2[i] = true;
+            } else {
+                array2[i] = false;
+            }
+        }
+
+        return array2;
     }
 }
